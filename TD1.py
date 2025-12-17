beb3dc8ba1e42f4424d6a3d24a08daf3c276cece
JohanVerne <johan.verne2003@gmail.com>
2025-12-17 09:21:22 +0100

    Init commit with previous course work

---
+++ TD1.py
@@ -0,0 +1,159 @@
+from abc import ABC, abstractmethod
+from collections import deque
+
+
+class Rootedgraph(ABC):
+    @abstractmethod
+    def roots(self):  # return all graph roots
+        pass
+
+    @abstractmethod
+    def neighbors(self, vertex):  # return all neighbors if vertex
+        pass
+
+
+class DictionaryGraph(Rootedgraph):
+    def __init__(self, graph=None, roots=None):
+        self.graph = graph if graph is not None else {}
+        self._roots = roots if roots is not None else []
+
+    def roots(self):
+        return self._roots
+
+    def neighbors(self, vertex):
+        return self.graph.get(vertex, [])
+
+
+class HanoiGraph(Rootedgraph):
+    def __init__(self, n_disks: int):
+        self.n_disks = n_disks
+        self._roots = [(tuple(range(n_disks, 0, -1)), (), ())]  # All disks on tower A
+
+    def roots(self):
+        if self.n_disks <= 0:
+            return [((), (), ())]  # No disks, all towers empty
+        return self._roots
+
+    def neighbors(self, vertex):
+        # vertex : state of the towers as a tuple of 3 tuples
+        neighbors = []
+        for current_tower in range(3):
+            if vertex[current_tower]:  # If the tower is not empty
+                disk = vertex[current_tower][-1]  # Get the top disk
+                for target_tower in range(3):
+                    if target_tower != current_tower and (
+                        not vertex[target_tower] or disk < vertex[target_tower][-1]
+                    ):  # Check if move is valid
+
+                        # Create new state by moving the disk
+                        new_towers = list(
+                            map(list, vertex)
+                        )  # Convert tuples to lists for mutability + deep copy
+                        # Convert state from tuples to lists for mutability
+                        new_towers[current_tower].pop()
+                        # Remove disk from current tower
+                        new_towers[target_tower].append(disk)
+                        # Add disk to target tower
+                        neighbors.append(
+                            (tuple(map(tuple, new_towers)))
+                        )  # Convert back to tuple of tuples and add to new states (neighbors)
+        return neighbors
+
+
+def on_entry_check4vertex(vertex, opaque: int):
+    """Appelée sur chaque nouveau sommet visité
+    Ici, on arrête la recherche après avoir visité 4 sommets
+
+    Args:
+        vertex : Le sommet qui vient d'être découvert
+        opaque : Données passées / accumulées (peut être n'importe quoi)
+
+    Returns:
+        (terminate, new_opaque) :
+            terminate : booléen - True pour arrêter BFS, False pour continuer
+            new_opaque : Valeur mise à jour de opaque
+    """
+    if opaque == 3:
+        return (True, opaque)
+    return (False, opaque + 1)
+
+
+def parents_lists(parents: dict, root):
+    path = []
+    current = root
+    while current is not None:
+        path.append(current)
+        current = parents[current]
+    path.reverse()
+    return path
+
+
+def BFS(graph: Rootedgraph, on_entry: callable, opaque=None, get_path=False):
+    marked = []
+    queue = deque()  # double ended queue : can pop from both ends
+    for root in graph.roots():
+        queue.append(root)
+    parents = {
+        root: None for root in graph.roots()
+    }  # Parent dictionnary to reconstruct paths of the BFS tree
+
+    while queue:
+        v = queue.popleft()
+        if v not in marked:
+            marked.append(v)
+            terminate, opaque = on_entry(v, opaque)
+            if terminate:
+                if get_path:
+                    return parents_lists(parents, v), marked
+                return marked
+            for neighbors in graph.neighbors(v):
+                queue.append(neighbors)
+                parents[neighbors] = (
+                    v if neighbors not in parents else parents[neighbors]
+                )
+    return marked
+
+
+if __name__ == "__main__":
+    graph = {0: [1, 2], 1: [0, 2], 2: [0, 3, 4, 5], 3: [2], 4: [2, 5], 5: [2]}
+    graph2 = {0: [1, 2], 1: [0, 2], 2: [1, 2, 3], 3: [2, 4], 4: [3], 6: [7], 7: [6]}
+    print("====== BFS =======")
+    print(BFS(DictionaryGraph(graph, [0]), on_entry_check4vertex, 0))
+    print(BFS(DictionaryGraph(graph, [0, 5]), on_entry_check4vertex, 0))
+    # print(BFS(DictionaryGraph(graph, [0, 5])))
+    # print(BFS(DictionaryGraph(graph2, [0, 7])))
+    # print(BFS(DictionaryGraph(graph2, [6])))
+    print("============")
+    print(BFS(DictionaryGraph(graph2, [0]), lambda v, o: (v == 3, o), 0))
+    print(BFS(DictionaryGraph(graph2, [0, 6]), lambda v, o: (v == 3, o), 0))
+
+    print("====== HANOI =======")
+    hanoi_graph = HanoiGraph(3)
+    print(
+        BFS(hanoi_graph, lambda v, o: (v == ((), (), (3, 2, 1)), o), get_path=True)[0]
+    )
+    print(
+        len(
+            BFS(hanoi_graph, lambda v, o: (v == ((), (), (3, 2, 1)), o), get_path=True)[
+                0
+            ]
+        )
+    )
+
+    hanoi_graph = HanoiGraph(6)
+    print(
+        BFS(
+            hanoi_graph,
+            lambda v, o: (v == ((), (), (6, 5, 4, 3, 2, 1)), o),
+            get_path=True,
+        )[0]
+    )
+    print(
+        len(
+            BFS(
+                hanoi_graph,
+                lambda v, o: (v == ((), (), (6, 5, 4, 3, 2, 1)), o),
+                get_path=True,
+            )[0]
+        )
+    )
